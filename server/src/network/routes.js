6f4609d7058ec9d091337c51fff1b695bab84f62
nickdpz <ndpastranzamora@gmail.com>
2021-08-03 21:29:41 -0500

    :bug: Fix network

--- server/src/network/routes.js
+++ server/src/network/routes.js
@@ -1,11 +1,13 @@
 const express = require('express'); //eslint-disable-line
 const users = require('../components/users/network');
-const records = require('../components/');
+const records = require('../components/records/network');
+const nodes = require('../components/nodes/network');
 const auth = require('../components/auth/network');
 const paths = (server) => {
     server.use('/users', users);
     server.use('/auth', auth);
-    server.use('/records', records);
+    server.use('/nodes', nodes);
+    server.use('/', records);
 };
 
 module.exports = paths;
