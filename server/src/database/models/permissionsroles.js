c4672945984ec7c607acd4d01a2424598bd287cc
nickdpz <ndpastranzamora@gmail.com>
2021-08-03 21:25:51 -0500

    :wrench: Refactor models database

--- server/src/database/models/permissionsroles.js
+++ server/src/database/models/permissionsroles.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            models.PermissionsRoles.belongsTo(models.Products, {
-                as: 'Products',
+            models.PermissionsRoles.belongsTo(models.Permissions, {
+                as: 'Permissions',
                 foreignKey: 'permissionId',
                 onDelete: 'CASCADE'
             });
